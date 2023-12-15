2683e560d3ce5ec550006cd1aaa9bd2dc8652f22
Dario <dariomadau@hotmail.com>
2023-12-15 23:05:07 +0100

    added information to main,py

--- main.py
+++ main.py
@@ -1,3 +1,22 @@
+"""
+Sony RAW Star Selector
+
+This Python script is designed for Sony photographers who shoot in RAW + JPEG formats.
+It helps you easily find and transfer all 5-star rated RAW images from your camera to a designated folder.
+
+Author: @dariocodes
+GitHub: https://github.com/dariocodes/rate-finder
+
+This script is released under the MIT License. See the bottom of this file for full details.
+
+Usage:
+1. Run the script using `python3 main.py`.
+2. Enter the pathname of the source directory.
+3. Enter the pathname of the destination directory.
+
+For more information and FAQs, refer to the README.md file in the repository.
+"""
+
 from PIL import Image
 import os
 import shutil
@@ -51,3 +70,27 @@ if __name__ == '__main__':
                 else:
                     print(f'the RAW equivalent of {photo.name} could not be found')
                     continue
+
+"""
+MIT License
+
+Copyright (c) [2023] [@dariocodes]
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+"""
